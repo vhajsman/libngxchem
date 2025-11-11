6c4e814b149d87fc365d2e93ad410f064302a6b9
vhajsman <1816.cookie1816@gmail.com>
2025-11-11 21:40:39 +0100

    modified:   include/libngxchem/Element.hpp

--- include/libngxchem/Element.hpp
+++ include/libngxchem/Element.hpp
@@ -7,9 +7,18 @@
 #include "ElementClass.hpp"
 
 namespace ngxchem {
-    const Element* findBySymbol(const std::string& symbol) {
+    inline const Element* findElementBySymbol(const std::string& symbol) {
         for(const auto& el: ngxchem::PERIODIC_TABLE)  {
             if(el.symbol == symbol)
+                return &el;
+        }
+
+        return nullptr;
+    }
+
+    inline const Element* findElementByAtomicNumber(const int atomic_no) {
+        for(const auto& el: ngxchem::PERIODIC_TABLE) {
+            if(el.atomicNumber == atomic_no)
                 return &el;
         }
 
