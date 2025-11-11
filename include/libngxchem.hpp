a0d135bd28ecffcc2d8b31398670aaa52c600a82
vhajsman <1816.cookie1816@gmail.com>
2025-11-11 21:40:27 +0100

    added a wrapper for 'internal' headers

---
+++ include/libngxchem.hpp
@@ -0,0 +1,9 @@
+#ifndef __LIBNGXCHEM_HPP
+#define __LIBNGXCHEM_HPP
+
+#include "libngxchem/Element.hpp"
+#include "libngxchem/ElementClass.hpp"
+#include "libngxchem/periodic_table.hpp"
+#include "libngxchem/temp_t.hpp"
+
+#endif
