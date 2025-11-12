a29dcd2bf7d4c2bdbd70395e2355f2d3786d96ab
vhajsman <1816.cookie1816@gmail.com>
2025-11-12 23:35:09 +0100

    added function for caluclating length

--- include/libngxchem/Vector3.hpp
+++ include/libngxchem/Vector3.hpp
@@ -1,12 +1,17 @@
 #ifndef __LIBNGXDEV_VECTOR3_HPP
 #define __LIBNGXDEV_VECTOR3_HPP
 
+#include <cmath>
 namespace ngxchem {
     struct Vector3 {
         double x, y, z;
 
         constexpr Vector3(): x(0), y(0), z(0) {}
         constexpr Vector3(double x, double y, double z): x(x), y(y), z(z) {}
+
+        constexpr double length() const {
+            return std::sqrt(x*x + y*y + z*z);
+        }
     };
 };
 
